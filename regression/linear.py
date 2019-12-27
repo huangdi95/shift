5126e7b600b680953aa0373e5c9067a99259b492
Huang Di <hd232508@163.com>
2019-12-27 21:27:38 +0800

    channel regression done!

--- regression/linear.py
+++ regression/linear.py
@@ -3,8 +3,7 @@ import statsmodels.api as sm
 import csv
 import matplotlib.pyplot as plt
 
-def linear_regression(x, y):
-    X = np.column_stack((x, 1/x))
+def linear_regression(y, X):
     X = sm.add_constant(X)
     mod = sm.OLS(y, X)
     res = mod.fit()
@@ -49,31 +48,69 @@ def preprocess(data_dict):
     data_dict.update({'time_act': new_time_act[1:]})
     return data_dict
     
-def figplt(data_x, data_y, params, start=0.1, end=512):
+def figplt(data_x, data_y, params, mode='nike', start=0.1, end=512):
     x = np.linspace(start, end, 1000, endpoint=True)
-    a = params[2]
-    b = params[1]
-    c = params[0]
-    y = a / x + b * x + c
+    if mode == 'nike':
+        a = params[1]
+        b = params[2]
+        c = params[0]
+        y = a / x + b * x + c
+    elif mode == 'linear':
+        a = params[1]
+        c = params[0]
+        y = a * x + c
     plt.plot(x, y)
     plt.scatter(data_x, data_y)
-    
 
-def main():
-    data_dict = read_csv('./V100-16GB.csv')
-    data_dict = preprocess(data_dict)
+def group_regression(data_dict, show_plot=False):
+    group_params_list = []
     labels = []
     for i in list(range(len(data_dict['group']))):
         labels.append(str([int(data_dict['c1'][i][0]), int(data_dict['c2'][i][0])]))
 #        print(data_dict['group'][i])
-        res = linear_regression(data_dict['group'][i][:-2], data_dict['time_act'][i][:-2])
+
+        x = data_dict['group'][i][:-2]
+        y = data_dict['time_act'][i][:-2]
+
+        X = np.column_stack((1/x, x))
+        res = linear_regression(y, X)
         if res.rsquared < 0.9:
             print(data_dict['c1'][i][0], data_dict['c2'][i][0])
             print('Parameters: ', res.params)
             print('R2: ', res.rsquared)
-        figplt(data_dict['group'][i][:-2], data_dict['time_act'][i][:-2], res.params)
-    plt.legend(labels=labels)
-    plt.show()
+        group_params_list.append(res.params)
+        if show_plot:
+            figplt(x, y, res.params)
+    if show_plot:
+        plt.legend(labels=labels)
+        plt.show()
+    return group_params_list 
+
+def channel_regression(data_dict, group_params_list, show_plot=False):
+    y = [] 
+    c1 = []
+    c2 = []
+    for i in list(range(len(group_params_list))):
+        c1.append(data_dict['c1'][i][0])
+        c2.append(data_dict['c2'][i][0])
+        y.append(group_params_list[i][1])
+    c1 = np.array(c1)
+    c2 = np.array(c2)
+    y = np.array(y)
+    X = c1 * c2
+    res = linear_regression(y, X)
+    print('Parameters: ', res.params)
+    print('R2: ', res.rsquared)
+    if show_plot:
+        figplt(X, y, res.params, 'linear', 0, 7e6)
+        plt.legend(labels='channel')
+        plt.show()
+
+def main():
+    data_dict = read_csv('./V100-16GB.csv')
+    data_dict = preprocess(data_dict)
+    group_result_list = group_regression(data_dict)
+    channel_regression(data_dict, group_result_list, True)
 
 if __name__ == '__main__':
     main()
