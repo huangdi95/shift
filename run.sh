52d02b0a097a4c481be175c2fcbc03f9392b9f2e
Huang Di <hd232508@163.com>
2019-12-30 11:27:44 +0800

    gpu type and channel loop order

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo "remove output.csv"
-rm -rf output.csv
-for c1 in 32 64 128 256 512 1024 2048 4096 8192
+gpu_id=3
+gpu_type="V100-16GB-"$gpu_id
+echo "remove "$gpu_type".csv"
+rm -rf $gpu_type.csv
+for c1 in 8192 4096 2048 1024 512 256 128 64 32
 do
-for c2 in 32 64 128 256 512 1024 2048 4096 8192
+for c2 in 8192 4096 2048 1024 512 256 128 64 32
 do
 for g in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
 do
@@ -11,8 +13,8 @@ if [ $g -gt $c1 -o $g -gt $c2 ]; then
 continue
 fi
 echo "running group="$g "c1="$c1 "c2="$c2
-CUDA_VISIBLE_DEVICES=2 \
-python measure_time.py --g=$g  --c1=$c1 --c2=$c2 \
+CUDA_VISIBLE_DEVICES=$gpu_id \
+python measure_time.py --group=$g  --c1=$c1 --c2=$c2 \
 2>&1 | tee run.log
 done
 done
