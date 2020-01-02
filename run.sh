811047e0236af1a03f1c6f1a920ade18a8f9fa4c
Huang Di <hd232508@163.com>
2020-01-02 15:14:48 +0800

    h w added; csvs dir added.

--- run.sh
+++ run.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
+groups=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+channel_in=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+channel_out=(1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192)
+height=(7 14 28 56 112 224 448 896)
+width=(7 14 28 56 112 224 448 896)
 for gpu_id in 1
 do
     gpu_type="V100-32GB-"$gpu_id
-    csv=$gpu_type".csv"
-    echo $csv
-    echo "remove "$gpu_type".csv"
-    rm -rf $gpu_type.csv
-    for c1 in 8192 4096 2048 1024 512 256 128 64 32
+    csv="csvs/"$gpu_type"-single.csv"
+    echo "remove "$csv
+    rm -rf $csv
+    for h in ${height[*]} 
     do
-        for c2 in 8192 4096 2048 1024 512 256 128 64 32
-        do
-            for g in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192
-            do
-                if [ $g -gt $c1 -o $g -gt $c2 ]; then
-                    continue
-                fi
-                echo "running group="$g "c1="$c1 "c2="$c2
-                CUDA_VISIBLE_DEVICES=$gpu_id \
-                python measure_time.py --group=$g --c1=$c1 --c2=$c2 --filename=$csv --gpu-type=$gpu_type \
-                2>&1 | tee $gpu_type.log
-            done
-        done
+    for w in ${width[*]} 
+    do
+    for c1 in ${channel_in[*]} 
+    do
+    for c2 in ${channel_out[*]} 
+    do
+    for g in ${groups[*]}
+    do
+        if [ $g -gt $c1 -o $g -gt $c2 ]; then
+            continue
+        fi
+        echo "running group="$g "h="$h "w="$w "c1="$c1 "c2="$c2
+        CUDA_VISIBLE_DEVICES=$gpu_id \
+        python measure_time.py --group=$g --h=$h --w=$w --c1=$c1 --c2=$c2 --filename=$csv --gpu-type=$gpu_type \
+        2>&1 | tee $gpu_type.log
+    done
+    done
+    done
+    done
     done
 done
